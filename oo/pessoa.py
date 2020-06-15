4393968a4dd9777cd89451003115d05d6f7b426a
randel92 <dimirandel@gmail.com>
2020-06-15 00:18:16 -0300

    adicionou metodo cumprimentar e alguns ex

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,9 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f"Ola {id(p)}"
+
+if __name__== '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p)) # igual a linha 9
+    print(id(p))
+    print(p.cumprimentar()) 
